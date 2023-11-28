4fedf8a8828e033ab561a60d225c86ccc5ca1a96
sueselle <sue@thehappyuser.com>
2023-11-28 05:14:55 -0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+var button = document.querySelector(".got-this");
+var modal = document.querySelector(".modal");
+var modalX = document.querySelector(".modal-x");
+var closeModal = function () {
+  modal.classList.remove("show-modal");
+  button.innerText = "Who's got this?";
+};
+
+button.addEventListener("click", function () {
+  modal.classList.add("show-modal");
+  button.innerText = "You've got this!!!";
+});
+
+modalX.addEventListener("click", function () {
+  modal.classList.remove("show-modal");
+  button.innerText = "Who's got this?";
+});
+
+document.addEventListener("keydown", function (e) {
+  //console.log(e.key);
+  if (e.key === "Escape") {
+    if (modal.classList.contains("show-modal")) {
+      closeModal();
+    }
+  }
+});
